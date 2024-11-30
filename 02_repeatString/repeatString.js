f7f8fb50d2f486e7cc13dd0dee137d14a3fbabd4
onurdemirk <96961148+onurdemirk@users.noreply.github.com>
2024-11-30 22:58:44 +0100

    Odin project JS exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,19 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    let finalString = "";
 
+    for (let i = 0; i <= num - 1; i++) {
+        if(num === 0) {
+            break;
+        }
+
+        finalString += string;
+    }
+    
+    if(num < 0) {
+        finalString = "ERROR";
+    }
+
+    return finalString;
 };
 
 // Do not edit below this line
